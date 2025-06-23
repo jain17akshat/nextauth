a40a6df7205f9e22eabab791a81fc0d63992eefa
jain17akshat <jain.7akshat@gmail.com>
2025-06-23 13:26:43 +0530

    Add initial login, signup pages, database configuration, and user model

---
+++ src/models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose form "mongoose";
+const userScehma=new mongoose.Schema({
+username:
+{
+    type:String,
+    required:[true,"Please provide username"],
+    unique:true
+
+},
+email:{
+    type:String,
+    required:[true,"Please provide email"],
+    unique:true
+
+},
+
+password:{
+    type:String,
+    required:[true,"Please provide password"],
+    unique:true
+
+},
+isVerified:{
+    type:Boolean,
+    default:false
+},
+isAdmin:{
+    type:Boolean,
+    default:false
+},
+
+
+forgotPaaswordToken:String,
+forgotPaaswordTokenTokenExpiry:Date,
+verifyToken:String,
+verifyTokenExpiry:Date, 
+})
+
+const User= mongoose.models.users|| mongoose.model("uers",userScehma)
+export default User
