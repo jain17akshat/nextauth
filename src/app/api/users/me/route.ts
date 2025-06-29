88149ab6a89a4f754a809fc8f307e3af99423cff
jain17akshat <jain.7akshat@gmail.com>
2025-06-29 14:46:52 +0530

    Implement user profile page and middleware for authentication; change user data retrieval to GET method and add redirection logic for protected routes.

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -7,7 +7,7 @@ import { connect } from "@/dbConfig/dbConfig";
 
 connect();
 
-export async function POST(request:NextRequest){
+export async function GET(request:NextRequest){
 
     try {
         const userId = await getDataFromToken(request);
